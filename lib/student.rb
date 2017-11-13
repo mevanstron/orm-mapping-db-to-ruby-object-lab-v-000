c7d2774d3df04baa8e72d199dcfa9e36638b8980
Michael Evans <michael@michaelgregoryevans.com>
2017-11-13 21:40:35 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -22,8 +22,6 @@ class Student
   end
 
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
     sql = <<-SQL
       SELECT * FROM students WHERE name = ?
       SQL
@@ -31,6 +29,14 @@ class Student
       self.new_from_db(row)
   end
 
+  def self.count_all_students_in_grade_9
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade = 9
+      SQL
+      array = DB[:conn].execute(sql)
+      binding.pry
+  end
+  
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
